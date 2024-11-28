05d056b24c8c18f08c6307aeeb03c2b2b45e8474
zic9494 <98081424+zic9494@users.noreply.github.com>
2024-11-28 23:55:46 +0800

    setting wedpack

--- HomePageGUI.js
+++
@@ -1,6 +0,0 @@
-import { setCookie,  getCookie} from "./Cookie.js"; 
-
-window.onload = function(){
-    console.log("Peko");
-    setCookie("Test", "peko", 2)
-}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { setCookie,  getCookie} from "./Cookie.js"; 
+
+window.onload = function(){
+    console.log("Peko");
+    setCookie("Test", 2, 2)
+}

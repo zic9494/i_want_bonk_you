c010fa30b50d833898bb730564eaf24b39bd1ca2
qwe638853 <qwe638853@gmail.com>
2024-11-29 02:47:53 +0800

    walletConnect

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 // import { setCookie,  getCookie} from "./cookie.js"; 
 // import { transaction } from "./transaction";
+import {connectWallet,disconnectWallet} from "./wallet.js"
+
 window.onload = function(){
+
+    document.getElementById('connect-wallet').addEventListener('click',connectWallet)
+    document.getElementById('disconnect-wallet').addEventListener('click',disconnectWallet)
 }
---
+++ src/wallet.js
@@ -0,0 +1,31 @@
+import * as solanaWeb3 from "@solana/web3.js"
+//Phantom文檔:https://docs.phantom.app/solana/establishing-a-connection
+
+export async function connectWallet(){  //連接錢包
+    if(window.solana && window.solana.isPhantom){
+        try{
+            const response = await window.solana.connect();
+            console.log("Address:",response.publicKey.toString());
+        }catch(err){
+            console.log("Error:",err);
+        }
+    }else{
+        console.log("not found wallet");
+    }
+}
+
+export async function disconnectWallet(){   //斷開連接
+    if(window.solana && window.solana.isPhantom){
+        try{
+            if(window.solana.isConnected){
+                await window.solana.disconnect();
+                console.log("Disconnected");
+            }  
+        }catch(err){
+            console.log("Error:",err);
+        }
+    }else{
+        console.log("not found wallet");
+    }
+}
+
